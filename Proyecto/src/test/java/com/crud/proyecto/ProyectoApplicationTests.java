7fe4e75c88deb735c88bd2eac257e63fceca359f
Xavich <bruno_vargas78@hotmail.com>
2020-08-28 23:30:27 -0500

    Commit inicial con eclipse

---
+++ Proyecto/src/test/java/com/crud/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crud.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
